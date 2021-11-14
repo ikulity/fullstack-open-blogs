935274d48cd66a02fd032fc029a6f5cccb99277b
Iikka <t8kuii00@students.oamk.fi>
2021-11-14 21:05:42 +0200

    Exercise 4.6

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -14,8 +14,35 @@ const favoriteBlog = (blogs) => {
     return favorite
 }
 
+const mostBlogs = (blogs) => {
+    let authors = []
+    blogs.forEach(blog => {
+        const authorName = blog.author
+        let isFound = false
+        authors.forEach(authorObject => {
+            if (authorObject.author === authorName) {
+                isFound = true
+                authorObject.blogs += 1
+                console.log(`Author ${authorName} was found! New blog count is ${authorObject.blogs}`)
+            }
+        })
+        if (!isFound) {
+            authors = authors.concat({ author: authorName, blogs: 1 })
+            console.log(`Author ${authorName} was not recorded, created a new object`)
+            console.log('Heres the new authors list:', authors)
+        } else {
+            console.log(`Author ${authorName} was recorded, and increment happened!`)
+        }
+    })
+    console.log("Authors and their blog counts", authors)
+    return authors.reduce((prev, current) => {
+        return (prev.blogs >= current.blogs) ? prev : current
+    }, 0)
+}
+
 module.exports = {
     dummy,
     totalLikes,
-    favoriteBlog
+    favoriteBlog,
+    mostBlogs
 }
