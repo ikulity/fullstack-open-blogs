65d85c6dfda3fd0afc6556241af86d1f165354fc
Iikka <t8kuii00@students.oamk.fi>
2021-11-15 15:27:45 +0200

    Exercise 4.15

---
+++ controllers/users.js
@@ -0,0 +1,37 @@
+const bcrypt = require('bcrypt')
+const usersRouter = require('express').Router()
+const User = require('../models/user')
+
+usersRouter.get('/', async (request, response) => {
+    const users = await User.find({})
+    response.json(users)
+})
+
+usersRouter.post('/', async (request, response) => {
+    const body = request.body
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(body.password, saltRounds)
+    const user = new User({
+        username: body.username,
+        name: body.name,
+        passwordHash
+    })
+    const savedUser = await user.save()
+    response.status(201).json(savedUser)
+})
+
+// these are copied routes from the blogsRouter
+// usersRouter.delete('/:id', async (request, response) => {
+//     const id = request.params.id
+//     const result = await User.findByIdAndRemove(id)
+//     response.status(200).json(result)
+// })
+
+// usersRouter.put('/:id', async (request, response) => {
+//     const id = request.params.id
+//     const updatedUser = { likes: request.body.likes }
+//     const result = await User.findByIdAndUpdate(id, updatedUser, { new: true })
+//     response.status(200).json(result)
+// })
+
+module.exports = usersRouter
