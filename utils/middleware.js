ba8329281b94e7e712fbdebf4e599d6935245e3c
Iikka <t8kuii00@students.oamk.fi>
2022-04-08 18:25:59 +0300

    Exercise 4.22 - move token extraction to separate middleware and use this middleware on blogRouter

--- utils/middleware.js
+++ utils/middleware.js
@@ -1,4 +1,6 @@
 const logger = require('./logger')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 const requestLogger = (req, res, next) => {
     logger.info('Method:', req.method)
@@ -34,9 +36,20 @@ const tokenExtractor = (req, res, next) => {
     next()
 }
 
+const userExtractor = async (req, res, next) => {
+    const token = req.token
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!decodedToken.id) {
+        return res.status(401).json({ error: 'token missing or invalid' })
+    }
+    req.user = await User.findById(decodedToken.id)
+    next()
+}
+
 module.exports = {
     requestLogger,
     unknownEndpoint,
     errorHandler,
-    tokenExtractor
+    tokenExtractor,
+    userExtractor,
 }
