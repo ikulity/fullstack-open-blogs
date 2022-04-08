02c29aed9b148ffc3daa65ae286207bcdcad7c92
Iikka <t8kuii00@students.oamk.fi>
2022-04-08 17:30:38 +0300

    Exercise 4.19 - add jsonwebtoken identification to adding new blogs. New blogs will have the userId added in

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,5 +1,7 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -7,8 +9,26 @@ blogsRouter.get('/', async (request, response) => {
 })
 
 blogsRouter.post('/', async (request, response) => {
-    const blog = new Blog({ ...request.body, user: '619d1608500c911f378c7766' })
+    const body = request.body
+    const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: 'token missing or invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
+
+    const blog = new Blog({ 
+        title: body.title,
+        url: body.url,
+        likes: body.likes,
+        author: body.author,
+        user: user._id,
+    })
+
     const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+
     response.status(201).json(savedBlog)
 })
 
@@ -24,5 +44,13 @@ blogsRouter.put('/:id', async (request, response) => {
     const result = await Blog.findByIdAndUpdate(id, updatedBlog, { new: true })
     response.status(200).json(result)
 })
+
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+        return authorization.substring(7)
+    }
+    return null
+}
 
 module.exports = blogsRouter
